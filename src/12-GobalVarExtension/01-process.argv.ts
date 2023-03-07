6524031fda82f5eacb118a3f679a85df95c10198
lengbingzi <lengbingzi@163.com>
2023-03-07 15:05:53 +0800

    调整目录结构

---
+++ src/12-GobalVarExtension/01-process.argv.ts
@@ -0,0 +1,12 @@
+// ts-node 01-process.argv.ts --a -b -c
+
+console.log(process.argv)
+
+//访问process.argv我们能轻松愉快的接收通过命令执行node程序时候所传入的参数。
+// [
+//   'C:\\Program Files\\nodejs\\node_modules\\ts-node\\dist\\bin.js',
+//   'D:\\DevelopData\\MyGitHub\\nodejs_learn\\src\\12-node内部变量\\01-process.argv.ts',
+//   '--a',
+//   '-b',
+//   '-c'
+// ]
